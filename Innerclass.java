9fc0101f3192fc51ab386064ef763fad4df56b1b
Bhupathi-Raju <bhupathi.raju@zemosolabs.com>
2017-07-04 11:30:36 +0530

    Update Innerclass.java

--- Innerclass.java
+++ Innerclass.java
@@ -1,20 +1,31 @@
 /**
  * Created by zemoso on 3/7/17.
  */
-public class Innerclass {
-    class In1
+class Outerclass {
+     class In1
     {
         public In1(String s)
         {
-            System.out.println("This is a Innerclass"+s);
+            System.out.println("This is called with object of "+s);
         }
     }
 }
-class Innerclass2 extends Innerclass{
-    class In2 extends Innerclass.In1
+class Outerclas2{
+    class In2 extends Outerclass.In1
     {
         public In2(String s) {
-            super(s);
+            new Outerclass().super(s);
         }
+    }
+}
+
+public class Run
+{
+    public static void main(String args[])
+    {
+        Outerclas2 o2 =new Outerclas2();
+        Outerclas2.In2 in2 = o2.new In2("Innerclass 2");
+        Outerclass o1 = new Outerclass();
+        Outerclass.In1 in1 = o1.new In1("Innerclasss 1");
     }
 }
